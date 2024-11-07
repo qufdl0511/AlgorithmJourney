6ac53ce7bef4bfb82917badca2d8a2ed65ae0404
qufdl0511 <qufdl051111@gmail.com>
2024-11-07 21:55:27 +0900

    backjoon241107_1

--- Backjoon/8393. 합/합.py
+++ Backjoon/8393. 합/합.py
@@ -1,3 +1,4 @@
+# 방법1. for문 사용하기
 import sys
 
 n = int(sys.stdin.readline())
@@ -5,3 +6,16 @@ result = sum(i for i in range(n + 1))
 
 # 1부터 N까지의 합
 print(result)
+
+#방법2. 수학 공식 사용하기
+import sys
+
+n = int(sys.stdin.readline())
+result = n * (n + 1) // 2  # 정수 나눗셈
+
+# 1부터 N까지의 합
+print(result)
+
+# 시간복잡도의 차이
+# for문은 0부터 n+1까지 n번 순회하기 때문에 시간복잡도는 O(n)
+# 수학 공식은 단순히 1번의 연산으로 n의 크기와 상관없으므로 시간복잡도는 O(1)
